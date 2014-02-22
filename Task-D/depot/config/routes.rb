d548c6efa8360d9c06f98c3e1259281a96ae04a2
Shawn <shawnresene@pluto.dsu.edu>
2014-02-21 18:09:53 -0600

    Task-D

---
+++ Task-D/depot/config/routes.rb
@@ -0,0 +1,20 @@
+Depot::Application.routes.draw do
+  
+get "store/index"
+  
+resources :products
+
+  
+
+
+
+# The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  
+
+root 'store#index', as: 'store'
+
+end
+
